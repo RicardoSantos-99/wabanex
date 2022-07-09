6f12a5016a4abacd74aa14188dd6f799fe41eec3
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-09 11:09:08 -0300

    feat: add imc calculate

--- lib/wabanex/imc.ex
+++ lib/wabanex/imc.ex
@@ -1,14 +1,20 @@
 defmodule Wabanex.Imc do
   def calculate_imc(filename) do
-    File.read(filename)
+    filename
+    |> File.read()
     |> handle_file()
   end
 
+  defp handle_file({:error, _reason}), do: {:error, "Error while reading file"}
+
   defp handle_file({:ok, content}) do
     content
+    |> String.split("\n")
+    |> Enum.map(fn line -> parse_line(line) end)
   end
 
-  defp handle_file({:error, reason}) do
-    reason
+  defp parse_line(line) do
+    line
+    |> String.split(",")
   end
 end
