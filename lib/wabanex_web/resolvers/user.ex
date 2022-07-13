c69ee11fd333ebbf968da106bf76e6d01ccce30d
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-13 20:57:18 -0300

    feat: add create user resolver

--- lib/wabanex_web/resolvers/user.ex
+++ lib/wabanex_web/resolvers/user.ex
@@ -1,3 +1,4 @@
 defmodule WabanexWeb.Resolvers.User do
   def get(%{id: user_id}, _context), do: Wabanex.getUser(user_id)
+  def create(%{input: params}, _context), do: Wabanex.createUser(params)
 end
