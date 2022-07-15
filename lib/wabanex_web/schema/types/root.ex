0f75a8325af2356a7b4feaf3cc3e1f2f647f4be9
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-14 22:00:57 -0300

    feat: add middlware error

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -2,6 +2,7 @@ defmodule WabanexWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
   alias WabanexWeb.Resolvers.User, as: UserResolver
+  alias Crudry.Middlewares.TranslateErrors
 
   import_types WabanexWeb.Schema.Types.User
 
@@ -20,6 +21,7 @@ defmodule WabanexWeb.Schema.Types.Root do
 
       resolve &UserResolver.create/2
       # resolve fn params, context -> UserResolver.get(params, context) end
+      middleware TranslateErrors
     end
   end
 end
