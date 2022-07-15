1bd3361f12dceced160b00c1ca5ebf68dbf55527
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-14 22:00:43 -0300

    feat: add cudry dep

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Wabanex.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:absinthe, "~> 1.5"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:crudry, "~> 2.4.0"}
     ]
   end
 
