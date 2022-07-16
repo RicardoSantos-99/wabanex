d64a1d9f884b79ba934aed0ab10fdb48355fcb93
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-15 21:00:46 -0300

    feat: add delete in defdelegate

--- lib/wabanex.ex
+++ lib/wabanex.ex
@@ -1,7 +1,9 @@
 defmodule Wabanex do
   alias Wabanex.Users.Create, as: CreateUser
   alias Wabanex.Users.Get, as: GetUser
+  alias Wabanex.Users.Delete, as: DeleteUser
 
   defdelegate createUser(params), to: CreateUser, as: :call
   defdelegate getUser(id), to: GetUser, as: :call
+  defdelegate deleteUser(id), to: DeleteUser, as: :call
 end
