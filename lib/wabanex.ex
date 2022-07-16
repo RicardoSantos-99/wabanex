5e8f9fcaf33f765592aa605e47c37ed5d63073c8
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-16 19:18:59 -0300

    feat: get users

--- lib/wabanex.ex
+++ lib/wabanex.ex
@@ -2,8 +2,10 @@ defmodule Wabanex do
   alias Wabanex.Users.Create, as: CreateUser
   alias Wabanex.Users.Get, as: GetUser
   alias Wabanex.Users.Delete, as: DeleteUser
+  alias Wabanex.Users.List, as: ListUsers
 
   defdelegate createUser(params), to: CreateUser, as: :call
   defdelegate getUser(id), to: GetUser, as: :call
   defdelegate deleteUser(id), to: DeleteUser, as: :call
+  defdelegate listUsers, to: ListUsers, as: :call
 end
