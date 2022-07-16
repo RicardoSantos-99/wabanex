88b8c793edd8a114237489909c2d817c24f69752
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-16 19:20:03 -0300

    feat: add delete user and list users resolvers

--- lib/wabanex_web/resolvers/user.ex
+++ lib/wabanex_web/resolvers/user.ex
@@ -1,4 +1,6 @@
 defmodule WabanexWeb.Resolvers.User do
   def get(%{id: user_id}, _context), do: Wabanex.getUser(user_id)
   def create(%{input: params}, _context), do: Wabanex.createUser(params)
+  def delete(%{id: user_id}, _context), do: Wabanex.deleteUser(user_id)
+  def list(__params, _context), do: Wabanex.listUsers()
 end
