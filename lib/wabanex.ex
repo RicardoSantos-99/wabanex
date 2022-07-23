0c74377cdebf966e81d04f5faa903a24b660b52c
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-23 16:43:45 -0300

    feat: add exercise

--- lib/wabanex.ex
+++ lib/wabanex.ex
@@ -4,8 +4,12 @@ defmodule Wabanex do
   alias Wabanex.Users.Delete, as: DeleteUser
   alias Wabanex.Users.List, as: ListUsers
 
+  alias Wabanex.Trainings.Create, as: CreateTraining
+
   defdelegate createUser(params), to: CreateUser, as: :call
   defdelegate getUser(id), to: GetUser, as: :call
   defdelegate deleteUser(id), to: DeleteUser, as: :call
   defdelegate listUsers, to: ListUsers, as: :call
+
+  defdelegate createTraining(params), to: CreateTraining, as: :call
 end
--- lib/wabanex_web/schema.ex
+++ lib/wabanex_web/schema.ex
@@ -5,11 +5,15 @@ defmodule WabanexWeb.Schema do
   import_types WabanexWeb.Queries.User
   import_types WabanexWeb.Schema.Types.User
 
+  import_types WabanexWeb.Schema.Types.Training
+  import_types WabanexWeb.Mutations.Training
+
   query do
     import_fields :user_queries
   end
 
   mutation do
     import_fields :user_mutation
+    import_fields :training_mutation
   end
 end
