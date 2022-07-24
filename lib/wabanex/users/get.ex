adc47c72ec9ae07cf12c049fc3a8dbbde3807bde
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-07-24 19:32:43 -0300

    feat: filter trainigs

--- lib/wabanex/users/get.ex
+++ lib/wabanex/users/get.ex
@@ -1,5 +1,6 @@
 defmodule Wabanex.Users.Get do
-  alias Wabanex.{User, Repo}
+  import Ecto.Query
+  alias Wabanex.{User, Repo, Training}
   alias Ecto.UUID
   # 7ec8517b-0c76-4b9e-b7c1-c24b5841d924
   def call(id) do
@@ -13,7 +14,17 @@ defmodule Wabanex.Users.Get do
   defp handle_response({:ok, uuid}) do
     case Repo.get(User, uuid) do
       nil -> {:error, "User not found"}
-      user -> {:ok, user}
+      user -> {:ok, load_trainings(user)}
     end
+  end
+
+  defp load_trainings(user) do
+    today = Date.utc_today()
+
+    query =
+      from t in Training,
+        where: ^today >= t.start_date and ^today <= t.end_date
+
+    Repo.preload(user, trainings: {first(query, :inserted_at), :exercises})
   end
 end
